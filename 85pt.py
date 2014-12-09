e3b05f4a8bb7e76794737476865600b0c7be092e
AsahelF <s651032@student.roundrockisd.org>
2014-12-09 13:38:16 -0600

    85

--- 85pt.py
+++ 85pt.py
@@ -14,7 +14,7 @@ root = Tk()
 # we can access them later on
 drawpadwidth = 480
 drawpadheight = 320
-drawpad = Canvas(root, width=drawpadwidth, height=drawpadheight, background='white')
+drawpad = Canvas(root, width=drawpadwidth, height=drawpadheight, background='black')
 oval = drawpad.create_oval(160,160,320,320, fill="red")
 
 class MyApp:
@@ -26,12 +26,17 @@ class MyApp:
 		self.myContainer1.pack()
 		
 		self.button1 = Button(self.myContainer1)
-		self.button1.configure(text="Left", background= "green")
+		self.button1.configure(text="Left", background= "purple")
 		self.button1.grid(row=0,column=0)
+		
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text='Right',background='yellow')
+		self.button2.grid(row=0,column=1)
 		
 	
 		# "Bind" an action to the first button												
 		self.button1.bind("<Button-1>", self.button1Click)
+		self.button2.bind('<Button-1>', self.button2Click)
 		 
 		  
 		# This creates the drawpad - no need to change this 
@@ -47,6 +52,18 @@ class MyApp:
 		global drawpad
 		global drawpadwidth
 		global drawpadheight
+		x1,y1,x2,y2 = drawpad.coords(oval)
+		if x1 > 0:
+		    drawpad.move(oval,-10,0)
+        def button2Click(self,event):
+            global oval
+            global drawpad
+            global drawpadwidth
+            x1,y1,x2,y2 = drawpad.coords(oval)
+            if x2 < drawpad.winfo_width():
+                drawpad.move(oval,10,0)
+		
+		    
 	
 	# Add the button2Click method
 		
